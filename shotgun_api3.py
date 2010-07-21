fce9ddafc83f75977248521f1676aa109fda1bf0
Isaac Reuben <isaac@isaac.local>
2010-07-21 15:28:47 -0700

    Update to license.

--- shotgun_api3.py
+++ shotgun_api3.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # ---------------------------------------------------------------------------------------------
 # Copyright (c) 2009-2010, Shotgun Software Inc
-# All rights reserved.
 # 
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
