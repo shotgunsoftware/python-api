d5b0b84fe56d3c6b154c19629381130089c2ce3c
Sylvain DELHOMME <syd@mikrosimage.eu>
2015-10-21 11:26:39 +0200

    Add rez package.py

---
+++ package.py
@@ -0,0 +1,12 @@
+name = 'shotgunPythonApi'
+version = '3.0.22'
+
+# Note for core pipeline team
+# Be sure that the same modifications are in both this api
+# and shotgun api included in tank core
+# eg. semi private shotgun support
+
+def commands():
+    
+    env.PYTHONPATH.append('{root}')
+
