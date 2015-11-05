4a352ca7ca8c7a9b18caf06143869d26637a0d57
KP <kp@shotgunsoftware.com>
2015-11-04 21:20:58 -0600

    initial commit for this **EXPERIMENTAL** branch Adds `ShotgunMulti` object which inherits from Shotgun and is returned when setting the multi_thread=True parameter when instantiating Shotgun. This object uses the requests module instead of httplib2/urllib. Adds callbacks to the upload() and download_attachment() methods which receive a ShotgunProgressMonitor object for use when handling progress info. More to come.

---
+++ shotgun_api3/lib/requests_toolbelt/exceptions.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""Collection of exceptions raised by requests-toolbelt."""
+
+
+class StreamingError(Exception):
+    """Used in :mod:`requests_toolbelt.downloadutils.stream`."""
+    pass
