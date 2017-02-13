977b3b0d2fa57ed4aaff1d588a2f342a76658c43
Jordi Riera <jriera@rodeofx.com>
2017-02-13 14:58:10 -0500

    mockgun: add percent in python_type

--- shotgun_api3/lib/mockgun/mockgun.py
+++ shotgun_api3/lib/mockgun/mockgun.py
@@ -460,6 +460,7 @@ class Shotgun(object):
                     python_type = {"number": int,
                                    "float": float,
                                    "checkbox": bool,
+                                   "percent": float,
                                    "text": basestring,
                                    "serializable": dict,
                                    "date": datetime.date,
