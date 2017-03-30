3b5595f81cc38a10ebaa089c59c64c2dfb37ff1a
Stephane Daigle <stephane.daigle@autodesk.com>
2017-03-30 15:43:16 -0400

    For #41856: added function to support new nav_search RPC

--- shotgun_api3/shotgun.py
+++ shotgun_api3/shotgun.py
@@ -2895,6 +2895,64 @@ class Shotgun(object):
             }
         )
 
+    def nav_search_string(self, project_id, search_string):
+        """
+        Search function adapted to work with the navigation hierarchy.
+
+        .. warning::
+
+            This is an experimental method that is not officially part of the
+            python-api. Usage of this method is discouraged. This method's name,
+            arguments, and argument types may change at any point.
+
+        :param int project_id: Id of the project in which the search will be done.
+        :param str search_string: String to search for.
+        :rtype: list: Found entries. Each array element is a dict containing the
+                        following keys:
+                        - str label: Name of the found entity
+                        - list incremental_path: List of paths leading to the entity.
+                                            These paths can be passed to the 'nav_expand'
+                                            function.
+                        - str path_label: User-friendly name of the path containing the
+                                            found entity.
+        """
+        return self._call_rpc(
+                'nav_search',
+                {
+                    'project_id':project_id,
+                    'search_criteria': { 'search_criteria' : { 'search_string': search_string }}
+                }
+        )
+
+    def nav_search_entity(self, project_id, entity):
+        """
+        Search function adapted to work with the navigation hierarchy.
+
+        .. warning::
+
+            This is an experimental method that is not officially part of the
+            python-api. Usage of this method is discouraged. This method's name,
+            arguments, and argument types may change at any point.
+
+        :param int project_id: Id of the project in which the search will be done.
+        :param dict entity: Reference to the entity to search for.
+        :rtype: list: Found entries. Each array element is a dict containing the
+                        following keys:
+                        - str label: Name of the found entity
+                        - list incremental_path: List of paths leading to the entity.
+                                            These paths can be passed to the 'nav_expand'
+                                            function.
+                        - str path_label: User-friendly name of the path containing the
+                                            found entity.
+        """
+        return self._call_rpc(
+                'nav_search',
+                {
+                    'project_id':project_id,
+                    'search_criteria':{ 'search_criteria' : {'entity': entity }}
+                }
+        )
+
     def get_session_token(self):
         """
         Get the session token associated with the current session.
