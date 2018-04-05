ebf09d2a60cba3da71a54cfa5fa6f2ae974742fb
Jean-François Boismenu <jean.francois.boismenu@autodesk.com>
2018-04-05 08:12:35 -0400

    appveyor build

---
+++ tests/run_appveyor.bat
@@ -0,0 +1,16 @@
+:: Copyright (c) 2018 Shotgun Software Inc.
+::
+:: CONFIDENTIAL AND PROPRIETARY
+::
+:: This work is provided "AS IS" and subject to the Shotgun Pipeline Toolkit
+:: Source Code License included in this distribution package. See LICENSE.
+:: By accessing, using, copying or modifying this work you indicate your
+:: agreement to the Shotgun Pipeline Toolkit Source Code License. All rights
+:: not expressly granted therein are reserved by Shotgun Software Inc.
+
+::
+:: This file is run by the appveyor builds.
+::
+
+%PYTHON%\Scripts\pip install nose
+%PYTHON%\Scripts\nosetests.exe -v
