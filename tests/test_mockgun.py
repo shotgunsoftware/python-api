f2c468201dcc7140918f27819d5e557073f5a358
Alex <alex@vitalityvfx.com>
2019-05-03 12:28:27 -0700

    Load the testing database for Mockgun
    Just has a bunch of the default projects from a shotgun site setup.

--- tests/test_mockgun.py
+++ tests/test_mockgun.py
@@ -51,6 +51,9 @@ Mockgun.set_schema_paths(
     os.path.join(mockgun_schema_folder, "schema.pickle"),
     os.path.join(mockgun_schema_folder, "schema_entity.pickle")
 )
+Mockgun.set_database_path(
+    os.path.join(mockgun_schema_folder, "data.pickle"),
+)
 
 
 # FIXME: This should probably be refactored into a base class for
