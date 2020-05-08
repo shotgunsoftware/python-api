1a245a48a311b1df731bc31893999040f43f7473
Benoît Gradit <benoit@gradit.fr>
2020-05-08 16:13:30 -0400

    Add timeout support on upload operations

--- shotgun_api3/shotgun.py
+++ shotgun_api3/shotgun.py
@@ -3976,7 +3976,10 @@ class Shotgun(object):
             request.add_header("Content-Type", content_type)
             request.add_header("Content-Length", size)
             request.get_method = lambda: "PUT"
-            result = opener.open(request)
+            if self.config.timeout_secs is not None:
+                result = opener.open(request, timeout=self.config.timeout_secs)
+            else:
+                result = opener.open(request)
             etag = result.info()["Etag"]
         except urllib.error.HTTPError as e:
             if e.code == 500:
@@ -4069,7 +4072,10 @@ class Shotgun(object):
 
         # Perform the request
         try:
-            resp = opener.open(url, params)
+            if self.config.timeout_secs is not None:
+                resp = opener.open(url, params, timeout=self.config.timeout_secs)
+            else:
+                resp = opener.open(url, params)
             result = resp.read()
             # response headers are in str(resp.info()).splitlines()
         except urllib.error.HTTPError as e:
