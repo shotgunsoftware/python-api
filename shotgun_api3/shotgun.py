97910abe796b9421a30f301c6c3027e322e028b1
Julien Langlois <julien.langlois@autodesk.com>
2025-10-24 12:14:33 -0700

    Modern Python

--- shotgun_api3/shotgun.py
+++ shotgun_api3/shotgun.py
@@ -4485,10 +4485,16 @@ class Shotgun(object):
             try:
                 opener = self._build_opener(urllib.request.HTTPHandler)
 
-                request = urllib.request.Request(storage_url, data=data)
-                request.add_header("Content-Type", content_type)
-                request.add_header("Content-Length", size)
-                request.get_method = lambda: "PUT"
+                request = urllib.request.Request(
+                    storage_url,
+                    method="PUT",
+                    headers={
+                        "Content-Type": content_type,
+                        "Content-Length": size,
+                    },
+                    data=data,
+                )
+
                 result = self._make_upload_request(request, opener)
 
                 LOG.debug("Completed request to %s", safe_short_url(storage_url))
