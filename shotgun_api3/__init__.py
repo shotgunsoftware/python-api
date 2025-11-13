01844be44c9a868d38fcb4eaada917f4537e021d
Julien Langlois <julien.langlois@autodesk.com>
2025-11-13 14:50:13 -0800

    Address review and update documentation

--- shotgun_api3/__init__.py
+++ shotgun_api3/__init__.py
@@ -8,15 +8,27 @@
 # agreement to the Shotgun Pipeline Toolkit Source Code License. All rights
 # not expressly granted therein are reserved by Shotgun Software Inc.
 
+import os
 import sys
 import warnings
 
 if sys.version_info < (3, 7):
-    raise Exception("This module requires Python version 3.7 or higher.")
+    if os.environ.get("SHOTGUN_API_ALLOW_OLD_PYTHON", "0") != "1":
+        raise Exception("This module requires Python version 3.7 or higher.")
+
+    warnings.warn(
+        "Python versions older than 3.7 are no longer supported as of January "
+        "2023. Since the SHOTGUN_API_ALLOW_OLD_PYTHON variable is enabled, this "
+        "module is raising a warning instead of an exceptiom. "
+        "However, it is very likely that this module will not be able to work "
+        "on this Python version.",
+        UserWarning,
+        stacklevel=2,
+    )
 elif sys.version_info < (3, 9):
     warnings.warn(
-        "Python versions older than 3.9 are no longer supported since 2025-03 "
-        "and compatibility will be removed at any time after end of March 2026. "
+        "Python versions older than 3.9 are no longer supported as of March "
+        "2025 and compatibility will be discontinued after March 2026. "
         "Please update to Python 3.11 or any other supported version.",
         DeprecationWarning,
         stacklevel=2,
