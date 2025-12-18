70552ae11206cd0fa148f3f8fd5acf5730b7275e
Julien Langlois <julien.langlois@autodesk.com>
2025-12-18 15:52:53 -0800

    no more logs

--- tests/test_api.py
+++ tests/test_api.py
@@ -37,17 +37,17 @@ from . import base
 
 import logging
 
-root = logging.getLogger()
-root.setLevel(logging.DEBUG)
+# root = logging.getLogger()
+# root.setLevel(logging.DEBUG)
 
-handler = logging.StreamHandler(sys.stdout)
-handler.setLevel(logging.DEBUG)
-formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-handler.setFormatter(formatter)
-root.addHandler(handler)
+# handler = logging.StreamHandler(sys.stdout)
+# handler.setLevel(logging.DEBUG)
+# formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+# handler.setFormatter(formatter)
+# root.addHandler(handler)
 
-logger = logging.getLogger("shotgun_api3")
-logger.setLevel(logging.DEBUG)
+# logger = logging.getLogger("shotgun_api3")
+# logger.setLevel(logging.DEBUG)
 
 
 class TestShotgunApi(base.LiveTestBase):
