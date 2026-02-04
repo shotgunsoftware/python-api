f58595484692bd42341fe8a8747d05f8feb3764b
Toolkitty App <236704337+toolkitty-app[bot]@users.noreply.github.com>
2026-02-04 17:04:17 +0000

    Auto-updated documentation based on tag v3.10.0

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v3.9.2',
+    VERSION: 'v3.10.0',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
