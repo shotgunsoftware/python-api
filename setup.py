c4c47502afe2b26a5a08939b63016e57898aa75a
Jay Roebuck <jay.roebuck@autodesk.com>
2026-06-02 13:49:44 -0400

    update version in setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ license = f.read().strip()
 
 setup(
     name="shotgun_api3",
-    version="3.10.0",
+    version="3.10.2",
     description="Flow Production Tracking Python API",
     long_description=readme,
     author="Autodesk",
